0a65d5a7804d80ead7457479c23fe2591eb165cc
Adeday00 <118217404+Adeday00@users.noreply.github.com>
2024-12-18 12:25:25 -0500

    Initial Commit

---
+++ W2WTests/W2WTests.swift
@@ -0,0 +1,17 @@
+//
+//  W2WTests.swift
+//  W2WTests
+//
+//  Created by David Adekanbi on 12/18/24.
+//
+
+import Testing
+@testable import W2W
+
+struct W2WTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
